579bf103ceab21e35a0047ab642b52b31b38192b
Janm20 <jan.mackovic@datablast.eu>
2023-03-16 21:29:53 +0100

    Routing done and adding pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Events from './pages/Events/Events';
+import MyEvents from './pages/MyEvents/MyEvents';
+import AboutUs from './pages/AboutUs/AboutUs';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
-      <h2 className='header'>SpotASpot</h2>
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/events" element={<Events />}></Route>
+      <Route path="/myevents" element={<MyEvents />}></Route>
+      <Route path="/about" element={<AboutUs />}></Route>
+
+    </Routes>
+
+    </>
+    
+    
   )
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import Navbar from './components/Navbar/Navbar'
+
+import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar />
+    
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 )
