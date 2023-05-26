24940f0b2f435841bc894a7c4380307f84d63ce6
Jan Mačković <jmackovic@koncar-institut.hr>
2023-05-26 08:47:19 +0200

    fix createEvent

--- Frontend/components/CreateEventsForm/CreateEventsForm.jsx
+++ Frontend/components/CreateEventsForm/CreateEventsForm.jsx
@@ -7,6 +7,10 @@ import {
   Box,
   Typography,
   Container,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useRouter } from "next/router";
@@ -19,36 +23,65 @@ const CreateEventsForm = (props) => {
     },
   });
 
+  const [category, setCategory] = React.useState('');
+
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
+
   const router = useRouter();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const submitData = new FormData(event.currentTarget);
     const token = localStorage.getItem("token");
-    const response = await fetch(BASE_URL + "/api/v1/event/add", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        name: submitData.get("name"),
-        description: submitData.get("description"),
-        category: submitData.get("category"),
-        city: submitData.get("city"),
-        location: submitData.get("location"),
-        dateTime: submitData.get("dateTime"),
-        isAvailable: true,
-      }),
-    });
-    // console.log(response.status);
-    // console.log(await response.text());
-    if (!response.ok) {
-      console.log("error");
-      return;
+    const eventPost =async () => {
+      const response = await fetch(BASE_URL + "/api/v1/event/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          name: submitData.get("name"),
+          description: submitData.get("description"),
+          category: category,
+          city: submitData.get("city"),
+          location: submitData.get("location"),
+          dateTime: submitData.get("dateTime"),
+          isAvailable: true,
+        }),
+      });
+      // console.log(response.status);
+      // console.log(await response.text());
+      if (!response.ok) {
+        console.log("error");
+        return;
+      }
     }
 
+    const imageEventPost =async () => {
+      const response = await fetch(BASE_URL + "/api/v1/event/"+id+"/image", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          image:submitData.get("image")
+        }),
+      });
+      // console.log(response.status);
+      // console.log(await response.text());
+      if (!response.ok) {
+        console.log("error");
+        return;
+      }
+    }
+    eventPost();
+    /* imageEventPost(); */
     router.push("/");
   };
   return (
@@ -93,16 +126,27 @@ const CreateEventsForm = (props) => {
                 id="description"
                 autoComplete="current-description"
               />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="category"
-                label="Category"
-                name="category"
-                autoComplete="category"
-                autoFocus
-              />
+              
+
+                <FormControl fullWidth margin="normal">
+                <InputLabel id="category">Category</InputLabel>
+                <Select
+                  labelId="category"
+                  id="category"
+                  value={category}
+                  label="Category"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={'RADIONICA'}>Radionica</MenuItem>
+                  <MenuItem value={'KVIZ'}>Kviz</MenuItem>
+                  <MenuItem value={'KOMEDIJA'}>Komedija</MenuItem>
+                  <MenuItem value={'KONCERT'}>Koncert</MenuItem>
+                  <MenuItem value={'SPORT'}>Sport</MenuItem>
+                  <MenuItem value={'DOBROTVORNI'}>Dobrotvorni</MenuItem>
+                  <MenuItem value={'IGRA'}>Igra</MenuItem>
+                  <MenuItem value={'NATJECANJE'}>Natjecanje</MenuItem>
+                </Select>
+              </FormControl>
 
               <TextField
                 margin="normal"
@@ -138,6 +182,21 @@ const CreateEventsForm = (props) => {
                   shrink: true,
                 }}
               />
+              <Button
+                variant="contained"
+                component="label"
+              >
+                Upload Image
+                <input
+                  type="file"
+                  id="image"
+                  name="image"
+                  
+                  hidden
+                />
+              </Button>
+
+
 
               <Button
                 type="submit"
